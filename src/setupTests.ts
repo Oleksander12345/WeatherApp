633cde1900c14d137180b1f94b68bbf451f6a47e
salisnyak07 <salisnyak07@gmail.com>
2025-07-01 16:39:13 +0300

    Commit 3. Aded a little configurations

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,12 +1,7 @@
-// src/setupTests.ts
-
-// 1. Завантажує .env змінні до process.env
 import 'dotenv/config';
 
-// 2. Розширює matchers (наприклад: expect(...).toBeInTheDocument())
 import '@testing-library/jest-dom';
 
-// 3. Для сумісності з jsdom (де TextEncoder/Decoder не реалізовані повністю)
 import { TextEncoder, TextDecoder } from 'util';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
@@ -17,7 +12,6 @@ jest.mock('axios');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-// 5. Стандартна мок-відповідь перед кожним тестом
 beforeEach(() => {
   mockedAxios.get.mockReset();
   mockedAxios.get.mockResolvedValue({
